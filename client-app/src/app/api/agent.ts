ab01bcb012064c58e65ef8a877e8e71c1e58db2f
Matija Komorcec <matija.komorcec@gmail.com>
2019-12-11 20:53:28 +0100

    Added client side form validation

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -31,6 +31,7 @@ axios.interceptors.response.use(undefined, (error: any) => {
   if (status === 500) {
     toast.error("Server error - check the terminal for more info!");
   }
+  throw error;
 });
 
 const responseBody = (response: AxiosResponse) => response.data;
