0bc9993e49e12485225a4ae8ebada218d749df19
Matija Komorcec <matija.komorcec@gmail.com>
2019-12-16 19:36:55 +0100

    Add client side Login and Register

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,8 +2,25 @@ import axios, { AxiosResponse } from "axios";
 import { IActivity } from "../models/activity";
 import { history } from "../..";
 import { toast } from "react-toastify";
+import { IUser, IUserFormValues } from "../models/user";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
+
+// Global configuration for all requests that we make
+// For every request, check if we have the token already
+axios.interceptors.request.use(
+  config => {
+    const token = window.localStorage.getItem("jwt");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 // Global configuration for what to do when an error occurs
 // first parameter was undefined on purpose, to not handle success responses
@@ -31,7 +48,7 @@ axios.interceptors.response.use(undefined, (error: any) => {
   if (status === 500) {
     toast.error("Server error - check the terminal for more info!");
   }
-  throw error;
+  throw error.response;
 });
 
 const responseBody = (response: AxiosResponse) => response.data;
@@ -73,6 +90,15 @@ const Activities = {
   delete: (id: string) => requests.del(`/activities/${id}`)
 };
 
+const User = {
+  current: (): Promise<IUser> => requests.get("/user"),
+  login: (user: IUserFormValues): Promise<IUser> =>
+    requests.post(`/user/login`, user),
+  register: (user: IUserFormValues): Promise<IUser> =>
+    requests.post(`/user/register`, user)
+};
+
 export default {
-  Activities
+  Activities,
+  User
 };
